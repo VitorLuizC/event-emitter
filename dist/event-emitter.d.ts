aea891d9d4b1b14012514107eee4dee937bc687a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-11-02 00:48:24 -0300

    🏗 use bitty's strategy to transpile and bundle

--- dist/event-emitter.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Handler = (payload: any) => void;
-interface EventEmitter {
-    on(event: string, handler: Handler): EventEmitter;
-    off(event?: string, handler?: Handler): EventEmitter;
-    once(event: string, handler: Handler): EventEmitter;
-    emit(event: string, payload: any): EventEmitter;
-}
-declare const createEmitter: (name: any) => EventEmitter;
-export { createEmitter as default, createEmitter, EventEmitter, Handler };
---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import { terser } from 'rollup-plugin-terser';
+
+/**
+ * Creates an output options object.
+ * @param {import('rollup').OutputOptions} options
+ * @returns {import('rollup').OutputOptions}
+ */
+const Option = (options) => ({
+  exports: 'named',
+  sourcemap: true,
+  ...options,
+});
+
+/**
+ * An object with all configuration for `Rollup.js`.
+ * @type {import('rollup').RollupOptions}
+ */
+const options = {
+  input: './src/event-emitter.js',
+  output: [
+    Option({
+      file: './dist/event-emitter.js',
+      format: 'commonjs',
+    }),
+    Option({
+      file: './dist/event-emitter.esm.js',
+      format: 'esm',
+    }),
+    Option({
+      file: './dist/event-emitter.mjs',
+      format: 'esm',
+    }),
+    Option({
+      file: './dist/event-emitter.umd.js',
+      name: 'EventEmitter',
+      format: 'umd',
+    }),
+    Option({
+      file: './dist/event-emitter.umd.min.js',
+      name: 'EventEmitter',
+      format: 'umd',
+      plugins: [terser()],
+    }),
+  ],
+};
+
+export default options;
