8957032e9ec7187e308f4f6d11b3290c2b076d15
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-11-02 13:37:56 -0300

    ✅ add once unit tests

---
+++ src/once.spec.ts
@@ -0,0 +1,48 @@
+import test, { afterEach } from 'ava';
+import emit from './emit.js';
+import emitters from './emitters.js';
+import once from './once.js';
+
+let target: object;
+
+afterEach(() => {
+  if (target) {
+    emitters.delete(target);
+  }
+});
+
+test('once: attach event handler to event name', (context) => {
+  target = {};
+
+  const onAddStuff = () => {
+    context.pass();
+  };
+
+  once(target, 'add-stuff', onAddStuff);
+
+  emit(target, 'add-stuff', undefined);
+});
+
+test('once: event handler can only be called once', async (context) => {
+  target = {};
+
+  let onAddStuffCalledTimes = 0;
+
+  const onAddStuff = () => {
+    onAddStuffCalledTimes++;
+  };
+
+  once(target, 'add-stuff', onAddStuff);
+
+  emit(target, 'add-stuff', undefined);
+  emit(target, 'add-stuff', undefined);
+  emit(target, 'add-stuff', undefined);
+  emit(target, 'add-stuff', undefined);
+  emit(target, 'add-stuff', undefined);
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, 500);
+  });
+
+  context.is(onAddStuffCalledTimes, 1);
+});
